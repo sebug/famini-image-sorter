97c4ba46196d96c4d1f2b8e370b68fc61763bece
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2022-10-01 15:02:29 +0000

    Adding fetching environment variables

--- Program.cs
+++ Program.cs
@@ -1,1 +1,4 @@
 ﻿Console.WriteLine("Famini Image Sorter");
+var accountName = Environment.GetEnvironmentVariable("FAMINI_ACCOUNT_NAME") ?? throw new Exception("Define variable FAMINI_ACCOUNT_NAME");
+var accountKey = Environment.GetEnvironmentVariable("FAMINI_ACCOUNT_KEY") ?? throw new Exception("Define variable FAMINI_ACCOUNT_KEY");
+
