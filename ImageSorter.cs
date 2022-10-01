fb285c75bafce6e9f6d38244b9fe17e754d0a3b0
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2022-10-01 16:26:33 +0000

    Adding image sorter record

---
+++ ImageSorter.cs
@@ -0,0 +1,3 @@
+public record ImageSorter(string AccountName, string AccountKey) {
+    
+}
--- Program.cs
+++ Program.cs
@@ -2,3 +2,4 @@
 var accountName = Environment.GetEnvironmentVariable("FAMINI_ACCOUNT_NAME") ?? throw new Exception("Define variable FAMINI_ACCOUNT_NAME");
 var accountKey = Environment.GetEnvironmentVariable("FAMINI_ACCOUNT_KEY") ?? throw new Exception("Define variable FAMINI_ACCOUNT_KEY");
 
+var imageSorter = new ImageSorter(accountName, accountKey);
